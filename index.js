a93d67f12f110bce05d881f1922b4b718644a68d
JamesNg <njames15@gmail.com>
2022-04-25 21:48:22 -0400

    commit tree d4d9e92b28b66cc97d35051052e10bbf9b2260ef

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(d) {
+    return d > 42 ? d-42 : 42-d;
+}
+
+function distanceFromHqInFeet(d) {
+    const blocks = distanceFromHqInBlocks(d);
+    return blocks*264;
+}
+
+function distanceTravelledInFeet(b1, b2) {
+    return b1 < b2 ? (b2-b1)*264 : (b1-b2)*264;
+}
+
+function calculatesFarePrice(b1, b2) {
+    const feet = distanceTravelledInFeet(b1, b2);
+    if (feet < 400) {
+        return 0;
+    } else if (feet > 400 && feet < 2000) {
+        return (feet-400)*0.02;
+    } else if (feet > 2000 && feet < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
